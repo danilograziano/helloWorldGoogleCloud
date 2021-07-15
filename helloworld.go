dad80cad9cee60335bbc0e445d264da3919b56ef
Danilo Graziano <danilo.graziano@visura.it>
2021-07-15 12:10:12 +0200

    aggiunto cognome

--- helloworld.go
+++ helloworld.go
@@ -55,7 +55,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World from Danilo!")
+	fmt.Fprint(w, "Hello, World from Danilo Graziano!")
 }
 
 // [END indexHandler]
